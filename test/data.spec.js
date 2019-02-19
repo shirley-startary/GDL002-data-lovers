7af0b0ee033fdf683e753c0bb1de60be4c242d98
shirley-startary <shirley.startary@gmail.com>
2019-02-19 10:20:15 -0600

    Agrega data de rick and morty

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,30 @@
+const rickandmorty = require('../src/data/rickandmorty/rickandmorty.json');
 require('../src/data.js');
 
+describe('dataLover Object', () => {
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+  it('Debería ser un objeto', () => {
+    expect(typeof dataLover).toBe('object');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  describe('filter', () => {
+    it('Debería ser funcción', () => {
+      expect(typeof dataLover.filter).toBe('function');
+    });
+
+    it('returns array of object ', () => {
+      expect(dataLover.filter(rickandmorty.results, 'Male') instanceof Array).toBe(true);
+    });
+  });
+
+  describe('Sort', () => {
+    it('is a function', () => {
+      expect(typeof dataLover.sortData).toBe('function');
+    });
+
+    // it('returns array of object ', () => {
+    //   expect(dataLover.sortData(rickandmorty.results, 'Male') instanceof Array).toBe(true);
+    // });
   });
 });
+// "pretest": "npm run eslint && npm run htmlhint",
