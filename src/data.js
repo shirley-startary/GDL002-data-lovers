f0197608000eb0abfea7df2afff246130f4c2a7d
shirley-startary <shirley.startary@gmail.com>
2019-02-19 10:23:24 -0600

    agrega funcion showData y filter

--- src/data.js
+++ src/data.js
@@ -1,1 +1,21 @@
+const main = () => {
+  // console.log(`window.RICKANDMORTY`, window.RICKANDMORTY);
+  const showData = arrayData => {
+    // Esta función debe de recibir una arraglo de objetos
+    // Y debe de retornar una string de tarjetas.
+    let template = '';
+    arrayData.forEach(element => {
+      template += `<div>
+                    <img src="${element.image}"/>
+                    <p>${element.name}</p>
+                    <p>${element.species}</p>
+                  <div>`;
+    });
+    return template;
+  };
 
+  document.getElementById('root').innerHTML = showData(window.RICKANDMORTY.results);
+
+
+};
+window.addEventListener('load', main);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
+const dataLover = {
+  filter: (data, filterBy) =>
+    data.filter(element => {
+      if (filterBy == element.name || filterBy == element.gender || filterBy == element.species) {
+        return element;
+      }
+    }),
+  sortData: () => {}
 
+};
+
+window.dataLover = dataLover;
